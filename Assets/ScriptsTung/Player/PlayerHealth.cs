0638449d27def2ea6d7e3d1cce8347013f7b6ed0
Sadless1 <demonnot5@gmail.com>
2026-07-18 02:24:36 +0700

    fix PlayerHealth code

--- Assets/ScriptsTung/Player/PlayerHealth.cs
+++ Assets/ScriptsTung/Player/PlayerHealth.cs
@@ -1,24 +1,24 @@
 ﻿using UnityEngine;
 
-public class PlayerHealth1 : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
     public int health = 100;
     public int maxHealth = 100;
 
-    // Hàm bị quái đánh trúng
-    //public void TakeDamage(int damage)
-    //{
-    //    health -= damage;
-    //    Debug.Log("Player bị đánh! Máu còn: " + health);
+    //Hàm bị quái đánh trúng
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Player bị đánh! Máu còn: " + health);
 
-    //    if (health <= 0)
-    //    {
-    //        Debug.Log("Player chết!");
-    //        Destroy(gameObject); // Xoá player
-    //    }
-    //}
+        if (health <= 0)
+        {
+            Debug.Log("Player chết!");
+            Destroy(gameObject); // Xoá player
+        }
+    }
 
-    // Hàm hồi máu (khi ăn bình máu)
+    //Hàm hồi máu(khi ăn bình máu)
     public void Heal(int amount)
     {
         health += amount;
