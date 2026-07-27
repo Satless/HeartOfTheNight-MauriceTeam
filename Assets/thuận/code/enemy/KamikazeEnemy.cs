0f2d21db239b5d4957b95e8c0b9041e64ae3f57a
Thuan21014 <Ducthuanhn2k5@gmail.com>
2026-07-27 18:44:36 +0700

    Sửa nhẹ

--- Assets/thuận/code/enemy/KamikazeEnemy.cs
+++ Assets/thuận/code/enemy/KamikazeEnemy.cs
@@ -93,7 +93,7 @@ public class KamikazeEnemy : MonoBehaviour
             if (spriteRenderer != null)
             {
                 if (visible)
-                    spriteRenderer.color = Color.white;                 // Hiện bình thường
+                    spriteRenderer.color = Color.red;                 // Hiện bình thường
                 else
                     spriteRenderer.color = new Color(1, 1, 1, 0.2f);    // Mờ trắng
             }
