491ff628c636ebdc9115d40fa10863d5f9e6f67a
hn <nnh18122006@gmail.com>
2026-08-01 22:21:09 +0700

    audio mp3/waves + audio settings

--- Assets/Huy/Script_Huy/Audio/SFX/SoundManager.cs
+++ Assets/Huy/Script_Huy/Audio/SFX/SoundManager.cs
@@ -47,4 +47,18 @@ public class SoundManager : MonoBehaviour
             sfxSource.PlayOneShot(clip);
         }
     }
+
+    public void PlaySound2DButton(string fullPath)
+    {
+        // split chain follwing '/' (ex: "UI/Click" -> categoryID = "UI", soundName = "Click")
+        string[] parts = fullPath.Split('/');
+        if (parts.Length == 2)
+        {
+            PlaySound2D(parts[0], parts[1]);
+        }
+        else
+        {
+            Debug.LogWarning("Enter following the right order: 'CategoryID/SoundName' (Ex: UI/Click)");
+        }
+    }
 }
