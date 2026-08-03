e68adeb60188660b26a4cf07c977f69aaf008375
Thuan21014 <Ducthuanhn2k5@gmail.com>
2026-08-03 11:39:06 +0700

    kết nối menu với level

--- Assets/thuận/code/enemy/KamikazeEnemy.cs
+++ Assets/thuận/code/enemy/KamikazeEnemy.cs
@@ -125,7 +125,7 @@ public class KamikazeEnemy : MonoBehaviour
 
             if (distance <= explodeRange)
             {
-                PlayerHealth hp = player.GetComponent<PlayerHealth>();
+                TestPlayerHealth hp = player.GetComponent<TestPlayerHealth>();
 
                 if (hp != null)
                 {
