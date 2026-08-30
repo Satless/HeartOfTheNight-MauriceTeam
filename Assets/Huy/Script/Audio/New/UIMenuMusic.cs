5a028f8688ef6cdacd9f5cc7d5923e231b3f2fb6
hn <nnh18122006@gmail.com>
2026-08-31 01:27:55 +0700

    add more sfx + bgm

---
+++ Assets/Huy/Script/Audio/New/UIMenuMusic.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class UIMenuMusic : MonoBehaviour
+{
+    [Header("Menu Music Settings")]
+    [Tooltip("Tên track nhạc trong MusicLibrary_New dành riêng cho Menu này (VD: WinBGM, PauseBGM, GameOverBGM)")]
+    [SerializeField] private string menuTrackName = "DefaultBGM";
+    [SerializeField] private float fadeDuration = 0.3f;
+
+    private string previousTrackName = "";
+
+    private void OnEnable()
+    {
+        if (MusicManager_New.Instance != null)
+        {
+            // 1. Lưu lại thời gian bài nhạc màn chơi đang phát dở
+            MusicManager_New.Instance.SaveCurrentMusicTime();
+            previousTrackName = MusicManager_New.Instance.CurrentTrackName;
+        }
+
+        // 2. Chuyển sang nhạc riêng của Menu này
+        AudioEvents.TriggerMusic(menuTrackName, fadeDuration);
+    }
+
+    private void OnDisable()
+    {
+        // 3. Khi đóng Menu, tiếp tục phát bài nhạc cũ ngay tại thời điểm đã dừng
+        if (MusicManager_New.Instance != null && !string.IsNullOrEmpty(previousTrackName))
+        {
+            MusicManager_New.Instance.PlayMusicResume(previousTrackName, fadeDuration);
+        }
+    }
+}
